d152804f361a957a1f259a7109477241162d0e5d
JanZakrzewski <jan.zakrzewski.pl+git@gmail.com>
2015-12-03 22:58:02 +0100

    dapper contrib issue example

---
+++ DapperContrib144Issue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using DapperLib;
+
+namespace DapperContrib144Issue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection connection = null;
+            object entity = null;
+            DapperContribWrapper.Insert(connection, entity);
+        }
+    }
+}
---
+++ DapperLib/DapperContribWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using Dapper.Contrib.Extensions;
+
+namespace DapperLib
+{
+    public class DapperContribWrapper
+    {
+        public static void Insert<T>(IDbConnection connection, T entity) where T: class
+        {
+            connection.Insert(entity);
+        }
+    }
+}
